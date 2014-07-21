5d94807edf620c13cd97ac00ee50f617119e24a7
Zihao Zhang <geraldzhang1994@gmail.com>
2014-07-21 15:25:53 -0700

    Finished layout and route

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    #return the full title of a page
+    def full_title(page)
+        base_title = "RoR Tutorial"
+        if page.empty?
+            base_title
+        else
+            "#{base_title} | #{page}"
+        end
+    end
 end
